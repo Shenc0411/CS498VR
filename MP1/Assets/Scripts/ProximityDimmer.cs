24fc9c64979dd968f8a8f2bc3ba8af50791c33a0
Shenc0411 <shenc0411@gmail.com>
2017-09-07 14:46:20 -0500

    Fixed a bug in lighting script

--- MP1/Assets/Scripts/ProximityDimmer.cs
+++ MP1/Assets/Scripts/ProximityDimmer.cs
@@ -6,7 +6,7 @@ public class ProximityDimmer : MonoBehaviour {
 
     public Light pointLight;
     public Transform playerTransform;
-    private Vector3 lightPos;
+    private Vector3 lightPos = new Vector3(0, 5, 0);
 	// Update is called once per frame
 	void Update () {
         Vector3 playerPos = playerTransform.localPosition;
