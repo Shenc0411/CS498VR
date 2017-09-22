99aff6678d679fec9e435d89e318aaa7226fe41b
Cong Shen <shenc0411@gmail.com>
2017-09-22 13:48:41 -0500

    Start Working on MP2Part2

---
+++ MP2Part2/Assets/Scripts/ToggleTracking.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleTracking : MonoBehaviour {
+
+    public bool positionTracking = true;
+    public bool rotationTracking = true;
+    public Transform mainCameraParentTransform;
+    public Transform mainCameraTransform;
+
+    private Vector3 mainCameraOriginalPosition;
+    private Vector3 mainCameraParentOriginalPosition;
+    private Quaternion mainCameraOriginalRotation;
+    private Quaternion mainCameraParentOriginalRotation;
+	// Use this for initialization
+	void Start () {
+        mainCameraOriginalPosition = mainCameraTransform.position;
+        mainCameraParentOriginalPosition = mainCameraParentTransform.position;
+        mainCameraOriginalRotation = mainCameraTransform.rotation;
+        mainCameraParentOriginalRotation = mainCameraParentTransform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            positionTracking = !positionTracking;
+            Debug.Log("Position Tracking: " + positionTracking);
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            rotationTracking = !rotationTracking;
+            Debug.Log("Rotation Tracking: " + rotationTracking);
+        }
+        if (!positionTracking)
+        {
+
+        }
+        if (!positionTracking)
+        {
+            mainCameraParentTransform.position = mainCameraParentOriginalPosition + (mainCameraParentTransform.position - mainCameraOriginalPosition);
+        }
+	}
+}
