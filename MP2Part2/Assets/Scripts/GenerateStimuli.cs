ded1a379ccfbb7646ce21d524ccd8beed985f5e1
Cong Shen <shenc0411@gmail.com>
2017-10-06 15:53:27 -0500

    add part3

--- MP2Part2/Assets/Scripts/GenerateStimuli.cs
+++ MP2Part2/Assets/Scripts/GenerateStimuli.cs
@@ -2,18 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenerateStimuli : MonoBehaviour {
-    public Transform mainCameraTransform;
-    public Transform blue_oneTransform;
-    public Transform blue_twoTransform;
-    public Transform redTransform;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class GenerateStimuli : MonoBehaviour
+{
+
+    private bool isInit = false;
+    private bool isAppear = true;
+    private Vector3 RedBallSize, BlueBallOneSize, BlueBallTwoSize;
+    private float DisRedBall, DisBlueBallOne, DisBlueBallTwo;
+    private float TimeDelay = 0f;
+    private bool isChanging = false;
+
+    public Transform CameraLoc;
+    public Transform RedBall;
+    public Transform BlueBallOne;
+    public Transform BlueBallTwo;
+
+    // Use this for initialization
+    void Start()
+    {
+        RedBallSize = RedBall.localScale;
+        BlueBallOneSize = BlueBallOne.localScale;
+        BlueBallTwoSize = BlueBallTwo.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInit)
+        {
+            DisRedBall = Vector3.Distance(CameraLoc.position, RedBall.position);
+            DisBlueBallOne = Vector3.Distance(CameraLoc.position, BlueBallOne.position);
+            DisBlueBallTwo = Vector3.Distance(CameraLoc.position, BlueBallTwo.position);
+            if (isAppear)
+            {
+                if(!isChanging)
+                {
+                    BlueBallOne.localScale = RedBallSize * (DisBlueBallOne / DisRedBall);
+                    BlueBallTwo.localScale = RedBallSize * (DisBlueBallTwo / DisRedBall);
+                }
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    isAppear = false;
+                    RedBall.localScale = new Vector3(0, 0, 0);
+                    isChanging = true;
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    isAppear = true;
+                    RedBall.localScale = RedBallSize;
+                    isChanging = true;
+                }
+            }
+            if(isChanging)
+            {
+                TimeDelay = TimeDelay + Time.deltaTime;
+                if (TimeDelay >= 2f){
+                    if(!isAppear)
+                    {
+                        BlueBallOne.localScale = new Vector3(0, 0, 0);
+                        BlueBallTwo.localScale = new Vector3(0, 0, 0);
+                    }
+                    else if(isAppear)
+                    {
+                        BlueBallOne.localScale = RedBallSize * (DisBlueBallOne / DisRedBall);
+                        BlueBallTwo.localScale = RedBallSize * (DisBlueBallTwo / DisRedBall);
+                    }
+                    TimeDelay = 0f;
+                    isChanging = false;
+                }
+            }
+        }else if(Input.GetKeyDown(KeyCode.S))
+        {
+            isInit = true;
+        }
+    }
 }
+
