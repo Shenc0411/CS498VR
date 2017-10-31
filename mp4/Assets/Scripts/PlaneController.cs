8453367eeb0b4e194578422ef40e22ac4161356b
Cong Shen <shenc0411@gmail.com>
2017-10-31 17:52:47 -0500

    start working on mp4

---
+++ mp4/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour {
+
+    public GameObject plane;
+
+    public float velocity;
+    public float acceleration;
+    public float accelerationFactor;
+    public float fractionFactor;
+    public Quaternion direction;
+
+
+
+    private Transform planeTransform;
+    private Rigidbody planeRigidBody;
+	// Use this for initialization
+	void Start () {
+        velocity = 0;
+        acceleration = 0;
+        direction = Quaternion.identity;
+        planeTransform = plane.transform;
+        planeRigidBody = plane.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Update velocity
+        float rightTriggerAxis = Input.GetAxis("XBOX_Right_Trigger");
+        float leftTriggerAxis = Input.GetAxis("XBOX_Left_Trigger");
+        acceleration = (rightTriggerAxis - leftTriggerAxis) * accelerationFactor;
+        velocity += (acceleration - velocity * fractionFactor) * Time.deltaTime;
+        planeTransform.localPosition = planeTransform.localPosition + new Vector3(0, 0, velocity * Time.deltaTime);
+    }
+}
